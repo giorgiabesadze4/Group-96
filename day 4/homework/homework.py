d422774a4d99ee2b7f38dbaf7cedfe5d6a50fb8f
giorgiabesadze4 <giorgiabesadzeaa@gmail.com>
2026-02-16 20:52:50 +0400

    second commit

---
+++ day 4/homework/homework.py
@@ -0,0 +1,53 @@
+#x = 25           #სახელიდან ვერ მივხვდებით რას ნიშნავს ცვლადი
+#y = 'Giorgi'
+
+#a = 5     
+#b = 10        #კოდის წაკითხვა რთულია რადგან არ ვიცით რას ნიშნავს a, b, c.
+#c = a + b
+
+
+#data = 10      #ცვლადი იყო რიცხვი შემდეგ კი გახდა ტექსტი ამის გამო შეიძლება შეცდომა დავუშვათ
+#data = "ten"
+
+#debugging-ი ნიშნავს პროგრამირებაში შეცდომების მოძებნას და გასწორებას
+
+#python-ში გვხვდება 5 ტიპის error-ი syntax error-ი,name error-ი,type error-ი,value error-ი და index error-ი
+
+name = "Lika"
+print(name)
+
+number = 5
+text = "apples"
+result = "number "+ "text"
+print(result)
+
+name = "liKa"
+name2 = name + "4"
+
+first_user = "Lika"
+user2nd = "Giorgi"
+print(first_user)
+
+first_name = "Data"
+last_name = "random"
+
+first_user = "Lika"
+user2nd = "Giorgi"
+print(first_user)
+
+
+city = input('Enter your city: ')
+district = input('Enter your district: ')
+
+print(city)
+print(district)
+
+age = input('Your age:')
+print(f'You are {age} years old')
+
+
+Num1 = input('Number: ')
+
+Num2 = input('Number2: ')
+
+print(Num1+Num2)
