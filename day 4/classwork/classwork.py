643909579424a9e530f637e7011f9793e8576243
giorgiabesadze4 <giorgiabesadzeaa@gmail.com>
2026-02-17 16:51:07 +0400

    second commit

---
+++ day 4/classwork/classwork.py
@@ -0,0 +1,11 @@
+user1 = 'Nika' #არ შეიძლება ცვლადი დაიწყოს რიცხვით და კიდევ ორივე იგივე პრჭყალი უნდა იყოს
+user2 = 'Lika' #აქაც არ შეიძლება რიცკხვით დაწყება და უნდა იყოს ორივე იგივე პრჭყალი
+print(user2)  #2_user და 2_User ერთნაირი არ არის
+
+name = 'Giorgi'
+surname = 'Abesadze'
+addres = 'Tbilisi'
+age = '14'
+print(f'i am {name} {surname}, i live in {addres} and i am {age} years old')
+
+
