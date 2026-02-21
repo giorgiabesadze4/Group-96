6c6fa3eecf1b6368007629eeca1c1e78f6f1b3d5
giorgiabesadze4 <giorgiabesadzeaa@gmail.com>
2026-02-21 23:29:32 +0400

    second commit

---
+++ day 6/classwork/classwork.py
@@ -0,0 +1,20 @@
+a = int(input('First Number: '))
+b = int(input('Second Number '))
+
+print('მიმატება:', a + b)
+print('გამოკლება', a - b)
+print('გამრავლება', a * b)
+print('გაყოფა', a / b)
+print('მთელზე გაყოფა', a // b)
+print('ნაშთი', a % b)
+print('ხარისხი', a ** b)
+
+print(bool(int(True) - int(False) + int(False)))  #ეს გამოიტანს True-ს რადგან 
+ # true = 1, false = 0. 1 - 0 + 0 = 1 რომელიც უდრის True-ს
+
+ #data conversion ნიშნავს მონაცემის ტიპის შეცვლას მაგალითად
+ # a = int()
+ # b = float()
+ # c = str()
+ # d = bool()
+
