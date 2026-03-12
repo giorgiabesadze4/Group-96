98a99baa7d243fcc8bac5fd62924d1f4b39a15c9
giorgiabesadze4 <giorgiabesadzeaa@gmail.com>
2026-03-12 16:04:22 +0400

    second commit

---
+++ day 10/homework/homework.py
@@ -0,0 +1,46 @@
+for i in range(50, 201):
+    print(i)
+
+    for i in range(2, 101, 2):
+     print(i)
+
+     for i in range(101, 151, 2):
+      print(i)
+
+i = 1
+
+while i <= 50:
+    print(i)
+    i = i + 1
+
+i = 20
+
+while i <= 60:
+    print(i)
+    i = i + 5
+
+surname = input("Enter your surname: ")
+
+for letter in surname:
+    print(letter)
+
+stock = 300
+
+while stock > 0:
+    stock = stock - 2
+    print("You have bought the drink")
+    print("Remaining drinks:", stock)
+
+print("Out of stock")
+
+for i in range(50, 19, -3):
+    print(i)
+
+i = 150
+
+while i >= 0:
+    print(i)
+    i = i - 1
+
+for i in range(1,6):
+    print(i)
