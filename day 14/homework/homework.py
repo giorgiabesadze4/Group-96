b146835e0990bc7690db417e56d03fdce57fd5cf
giorgiabesadze4 <giorgiabesadzeaa@gmail.com>
2026-03-26 20:37:33 +0400

    second commit

---
+++ day 14/homework/homework.py
@@ -0,0 +1,19 @@
+phrase = "Python Programming"
+print(phrase[:6])
+print(phrase[7:])
+print(phrase[::3])
+
+text = "PythonSlicing"
+print(text[:6])
+print(text[::-1])
+print(text[::2])
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(letters[::2])
+print(letters[2:8:2])
+print(letters[:5][::-1])
+
+sentence = "Slicing makes Python powerful"
+print(sentence[14:20])
+print(sentence[-9:][::-1])
+print(sentence[::3])
